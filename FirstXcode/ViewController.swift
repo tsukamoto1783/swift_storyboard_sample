25c6fecd582a696f459745f42f6f0a2cd77951f2
Tsukamoto Yuto <yuto.tsukamoto@ncdc.co.jp>
2024-01-05 10:17:30 +0900

    Initial Commit

---
+++ FirstXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstXcode
+//
+//  Created by Tsukamoto Yuto on 2024/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
